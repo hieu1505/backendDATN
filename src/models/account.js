f48a9b8b3442a6208a0671486be0e918c44776fd
hieu1505 <hieuqbh15@gmail.com>
2023-06-14 14:54:58 +0700

    nhan nuoi2

--- src/models/account.js
+++ src/models/account.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       this.hasOne(models.Center,{foreignKey:'account_id',as:'center'})
       this.hasMany(models.Like, { foreignKey: 'acount_id', as: 'like' });;
       this.hasMany(models.Comment, { foreignKey: 'account_id', as: 'comment' })
-      this.hasMany(models.Donor, { foreignKey: 'acount_id', as: 'donor' });
+      this.hasMany(models.Donor, { foreignKey: 'account_id', as: 'donor' });
       this.hasMany(models.Notification, { foreignKey: 'account_id', as: 'notification' });
 
     }
